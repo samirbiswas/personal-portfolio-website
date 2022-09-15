6a3e40609a8fb9dc6a8f0028a162da6e5e2315cb
samirbiswas <samir99biswas@gmail.com>
2022-09-15 11:40:06 +0600

    upload update resume

--- src/components/Home/HeaderMain/HeaderMain.js
+++ src/components/Home/HeaderMain/HeaderMain.js
@@ -13,43 +13,43 @@ library.add(fab, faCheckSquare, faCoffee)
 const HeaderMain = () => {
     return (
         <div className='container'>
-           <div style={{marginTop:'10%'}} className="row ">
-              <div className="col-md-6 ">
+            <div style={{ marginTop: '10%' }} className="row ">
+                <div className="col-md-6 ">
                     <div className='text-light offset-md-1 resume-button'>
                         <h4 className='text-success '>Hello! I am </h4>
                         <h1 className='name-design'>SAMIR BISWAS</h1>
                         <h3 className='text-success'>Front End Developer</h3>
                         <p className='description pb-3'>A front-end web developer is responsible for implementing visual
-                         elements that users see and interact with in a web application.</p>
-                        <a style={{textDecoration: 'none'}} className="text-white bg-success p-3 rounded" href={filess} target="_blank">GET RESUME</a>
-                        <Link  style={{textDecoration: 'none'}} to='/about'>
-                        <a style={{textDecoration: 'none',marginLeft:'5px'}} className="text-white bg-success p-3 rounded" >ABOUT ME</a>
+                            elements that users see and interact with in a web application.</p>
+                        <a style={{ textDecoration: 'none' }} className="text-white bg-success p-3 rounded" href={filess} target="_blank">GET RESUME</a>
+                        <Link style={{ textDecoration: 'none' }} to='/about'>
+                            <a style={{ textDecoration: 'none', marginLeft: '5px' }} className="text-white bg-success p-3 rounded" >ABOUT ME</a>
                         </Link>
-                        
-                   
-                    </div>
-              </div>
-              <div className="col-md-6 pl-5">
-                  <img className='image-setting' src={picture} alt="samir"/>
-                  <h5 className='text-success follow-me'>Follow Me </h5>
-                  <div className="text-white d-flex  icon-settings">
 
-                  <li className='a-color'>
-                      <a  href="https://github.com/samirbiswas/" target="_blank"><FontAwesomeIcon icon={['fab', 'github']} /> </a>
-                  </li>
-                  <li className='a-color'>
-                      <a href="https://www.linkedin.com/in/samirbiswas99/" target="_blank"><FontAwesomeIcon icon={['fab', 'linkedin']} /> </a>
-                  </li>
-                  <li className='a-color'>
-                      <a href="https://medium.com/@samir99biswas" target="_blank"><FontAwesomeIcon icon={['fab', 'medium']} /> </a>
-                  </li>
-                  
-                      
-                  </div>
-                 
-              </div>
-           </div>
-           
+
+                    </div>
+                </div>
+                <div className="col-md-6 pl-5">
+                    <img className='image-setting' src={picture} alt="samir" />
+                    <h5 className='text-success follow-me'>Follow Me </h5>
+                    <div className="text-white d-flex  icon-settings">
+
+                        <li className='a-color'>
+                            <a href="https://github.com/samirbiswas/" target="_blank"><FontAwesomeIcon icon={['fab', 'github']} /> </a>
+                        </li>
+                        <li className='a-color'>
+                            <a href="https://www.linkedin.com/in/samirbiswas99/" target="_blank"><FontAwesomeIcon icon={['fab', 'linkedin']} /> </a>
+                        </li>
+                        <li className='a-color'>
+                            <a href="https://medium.com/@samir99biswas" target="_blank"><FontAwesomeIcon icon={['fab', 'medium']} /> </a>
+                        </li>
+
+
+                    </div>
+
+                </div>
+            </div>
+
         </div>
     );
 };
